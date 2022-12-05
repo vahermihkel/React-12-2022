edf3a211d8dd9b784080b8b9e38421f8cb9faa5d
mihkelvaher <mihkel.vaher@industry62.com>
2022-12-05 15:57:42 +0200

    uus reacti projekt

---
+++ veebipood/src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+// WARNING in [eslint]
+// src\App.js
+//   Line 1:8:  'logo' is defined but never used  no-unused-vars
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="Pilt" src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" alt="" />
+      <button className="nupp">Nupp</button>
+    </div>
+  );
+}
+
+export default App;
+
+// () <--- shift + 8
+// [] <--- alt + 8
+// {} <-- alt + 7
+
+// shift + 9 --> ()
+// || shift *
+// shift + ü --> []
+// ü  --> {}
